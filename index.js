9569afe0b0cedea23fc2d455da94ff00521a1aa3
Ramil Mufazzalov <aoho92@gmail.com>
2023-03-27 19:21:00 +0500

    add test npm module

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-let num = 5
-console.log(`Variable: ${num}`);
+const CC = require('currency-converter-lt')
+let currencyConverter = new CC({ from: "USD", to: "RUB", amount: 100 })
+currencyConverter.convert().then((response) => {
+    console.log(`Result: ${response}`) //or do something else
+})
