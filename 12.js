6818bbd11c4893f5ed91a8f700f8dec1ae1c6f1f
Ramil Mufazzalov <aoho92@gmail.com>
2023-04-07 17:21:22 +0500

    Обработка текста из формы

--- 12.js
+++ 12.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))//указываем статическую папку, чтоб стили работали, картинки, видео
 
 
@@ -18,12 +19,21 @@ app.get('/404.html', (req, res) => {
     res.render('404')
 })
 
-
 app.get('/user/:username', (req, res) => { //после : создаём динамические параметры
     let data = { username: req.params.username, hobbies: ['Cinema', 'PC gaming', 'Fishing'] }
     res.render('user', data)
     //используем динамические параметры (http://localhost:3000/user/test)
 })
+
+
+app.post('/check-user', (req, res) => {
+    let username = req.body.username
+    if (username == "")
+        return res.redirect('/')
+    else
+        return res.redirect('/user/' + username)
+})
+
 
 const PORT = 3000
 app.listen(PORT, () => {
